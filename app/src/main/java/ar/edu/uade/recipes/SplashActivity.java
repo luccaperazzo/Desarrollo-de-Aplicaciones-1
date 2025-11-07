e549ef73c3eb5cd80d2bb90b7b930451923e5081
luccaperazzo <luccaakorea@gmail.com>
2025-11-07 01:52:34 -0300

    Splash Screen API implemented

--- app/src/main/java/ar/edu/uade/recipes/SplashActivity.java
+++ app/src/main/java/ar/edu/uade/recipes/SplashActivity.java
@@ -2,35 +2,36 @@ package ar.edu.uade.recipes;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.splashscreen.SplashScreen;
 
 import ar.edu.uade.recipes.util.UserManager;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final int SPLASH_TIME_OUT = 1000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
-
-        // Aplicar tema guardado
         applyTheme();
+        setContentView(R.layout.activity_splash);
 
-        UserManager userManager = new UserManager(this);
+        new Handler().postDelayed(() -> {
+            UserManager userManager = new UserManager(SplashActivity.this);
 
-        if (userManager.isLoggedIn()) {
-            // Usuario logueado -> ir a la home
-            startActivity(new Intent(this, HomeActivity.class));
-        } else {
-            // Usuario no logueado -> ir al Login
-            startActivity(new Intent(this, LoginActivity.class));
-        }
-
-        // Cerramos Splash para que no se pueda volver
-        finish();
+            if (userManager.isLoggedIn()) {
+                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+            } else {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
+            finish();
+        }, SPLASH_TIME_OUT);
     }
-
     private void applyTheme() {
         boolean isDarkMode = getSharedPreferences("settings", MODE_PRIVATE).getBoolean("dark_mode", false);
         if (isDarkMode) {
